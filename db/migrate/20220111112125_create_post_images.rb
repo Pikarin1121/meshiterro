7278a3ae878cc901726d1cf866546bb4a78867fa
EC2 Default User <ec2-user@ip-172-31-85-127.ec2.internal>
2022-01-15 08:12:33 +0000

    [finish]4-16

--- db/migrate/20220111112125_create_post_images.rb
+++ db/migrate/20220111112125_create_post_images.rb
@@ -1,11 +1,7 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-<<<<<<< HEAD
       
-=======
-
->>>>>>> origin/main
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2022_01_12_043742) do
-=======
-ActiveRecord::Schema.define(version: 2022_01_11_112125) do
->>>>>>> origin/main
+ActiveRecord::Schema.define(version: 2022_01_13_123448) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,12 +43,10 @@ ActiveRecord::Schema.define(version: 2022_01_11_112125) do
   create_table "post_images", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-<<<<<<< HEAD
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
-=======
->>>>>>> origin/main
+    t.string "image_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_01_11_112125) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
